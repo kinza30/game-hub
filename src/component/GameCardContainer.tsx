d7edba96b40853ec9f26874ce3bfbf8f63f9fb25
kinza30 <kinzaamjad2002@gmail.com>
2025-06-05 21:29:46 +0500

    Display genres

--- src/component/GameCardContainer.tsx
+++ src/component/GameCardContainer.tsx
@@ -1,14 +1,14 @@
-import { Box } from '@chakra-ui/react'
-import  { ReactNode } from 'react'
-interface Props{
-    children:ReactNode
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
 }
-const GameCardContainer = ({children}:Props) => {
+const GameCardContainer = ({ children }: Props) => {
   return (
-  <Box width='300px' borderRadius={10} overflow='hidden'>
-    {children}
-  </Box>
-  )
-}
+    <Box borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
