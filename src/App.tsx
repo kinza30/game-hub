c35d355208b6554ef567c4fca2aa8fafaf98e6b7
kinza30 <kinzaamjad2002@gmail.com>
2025-06-06 14:21:06 +0500

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import { Genre } from "./hooks/useGeneres";
 import PlatformSelector from "./component/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./component/SortSelector";
+
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -27,7 +29,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
